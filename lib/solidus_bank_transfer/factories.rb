3caa5cd4442ca3fba8bee8f7c15294cb47e33a64
Fabrizio Monti <fabrizio.monti@welaika.com>
2019-03-29 15:49:36 +0100

    Add a simple factory

--- lib/solidus_bank_transfer/factories.rb
+++ lib/solidus_bank_transfer/factories.rb
@@ -3,4 +3,12 @@ FactoryBot.define do
   #
   # Example adding this to your spec_helper will load these Factories for use:
   # require 'solidus_bank_transfer/factories'
+
+  factory :bank_transfer_payment_method, class: Spree::PaymentMethod::BankTransfer do
+    name 'Bank Transfer'
+    preferred_bank_name 'Uncle Scrooge Bank & Partners'
+    preferred_iban 'IT00 S000 0000 0000 0000 0123 456'
+    preferred_holder 'Donald Duck'
+    description 'Bank transfer'
+  end
 end
