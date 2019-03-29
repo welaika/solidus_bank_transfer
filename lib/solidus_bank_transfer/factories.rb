b736cbb3b377851bca67ad3aafd4fa3e796c3501
Fabrizio Monti <fabrizio.monti@welaika.com>
2019-03-29 18:37:45 +0100

    Must use blocks for factories

--- lib/solidus_bank_transfer/factories.rb
+++ lib/solidus_bank_transfer/factories.rb
@@ -7,11 +7,11 @@ FactoryBot.define do
   # require 'solidus_bank_transfer/factories'
 
   factory :bank_transfer_payment_method, class: Spree::PaymentMethod::BankTransfer do
-    name 'Bank Transfer'
-    preferred_bank_name 'Uncle Scrooge Bank & Partners'
-    preferred_iban 'IT00 S000 0000 0000 0000 0123 456'
-    preferred_holder 'Donald Duck'
-    description 'Bank transfer'
-    active true
+    name { 'Bank Transfer' }
+    preferred_bank_name { 'Uncle Scrooge Bank & Partners' }
+    preferred_iban { 'IT00 S000 0000 0000 0000 0123 456' }
+    preferred_holder { 'Donald Duck' }
+    description { 'Bank transfer' }
+    active { true }
   end
 end
