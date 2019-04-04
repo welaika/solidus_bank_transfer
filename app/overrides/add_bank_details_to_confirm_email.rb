59c12a3e65360827d2d2e2050e0d211099192ec8
Fabrizio Monti <fabrizio.monti@welaika.com>
2019-04-04 12:24:45 +0200

    Remove overrides: try not to use deface anymore

--- app/overrides/add_bank_details_to_confirm_email.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-Deface::Override.new(
-  virtual_path: 'spree/order_mailer/confirm_email',
-  name: 'add_bank_details_to_confirm_email',
-  insert_bottom: "[data-hook='payment-method']",
-  partial: 'spree/order_mailer/bank_transfer_details'
-)
--- app/overrides/add_bank_details_to_order_show.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-Deface::Override.new(
-  virtual_path: 'spree/shared/_order_details',
-  name: 'add_bank_details_to_order_show',
-  insert_bottom: '.payment-info',
-  partial: 'spree/orders/bank_transfer_details'
-)
